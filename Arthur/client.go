92e8254eac4a87020f2e414cd96ba1e85cce24e6
Arthur Guimarães <warthur2@gmail.com>
2013-08-23 14:42:23 -0300

    cliente quase

---
+++ Arthur/client.go
@@ -0,0 +1,146 @@
+package main
+
+import ( 
+  "net"     // provides the Listener and Conn types that hide many of the details of setting up socket connections
+//  "bufio"   // provides buffered read methods, simplifying common tasks like reading lines from a socket
+  "strconv" // function Itoa() that converts an integer to a string
+  "fmt"     // for printing strings to the console
+  "bytes"   // Compare bytes
+  "crypto/cipher"
+  "crypto/rand"
+  "encoding/hex"
+  "crypto/aes"
+  "io"
+)
+
+const S_PORT = 8000 // port that the server is going to listen
+const S_ADDR = "10.16.1.14:"
+const KEY = "Example Key 1234"
+
+func main() {
+
+  fmt.Printf("Initializing Client...\n")
+  
+  server, err := net.Dial("tcp", S_ADDR + strconv.Itoa(S_PORT))
+  if err != nil {
+    fmt.Printf("Connection Refused: ")
+	  panic(err)// handle error
+  }  
+  fmt.Printf("Use $unsec for secure connections or $sec for unsecure:\n")
+  Switch(server)
+  
+}
+
+func Switch(server net.Conn){
+  var message string
+  continue_ch := make(chan int, 1)
+  for{
+    fmt.Scan(&message)
+    if(message == "$unsec"){
+      fmt.Printf("Unsecure connection:\n\n")
+      go HandleUnsec(server, continue_ch)
+    } else if(message == "$sec"){
+      fmt.Printf("Secure connection:\n")
+     // go HandleSec()
+    }
+    <- continue_ch
+  }
+
+}
+
+func HandleUnsec(server net.Conn, continue_ch chan int){
+    server.Write([]byte("unsec\n"))
+
+    var msg = make([]byte, 512)
+	  server.Read(msg)
+   
+    n := bytes.Index(msg, []byte{0})
+    s := string(msg[:n])
+    fmt.Printf(s+"\n")
+ 
+    fmt.Printf("Now you can send your unsecure messages:\n")
+    var message string
+
+    for{
+        fmt.Scan(&message)
+        if(message == "$sec"){
+           break 
+        }           
+    }
+    continue_ch <- 1
+}
+
+func HandleSec(client net.Conn){
+}
+
+
+func Encrypter(plainText string, key string) string {
+	keyByte := []byte(key)
+	plaintext := []byte(plainText)
+        if len(plaintext)%aes.BlockSize != 0 {
+		panic("plaintext is not a multiple of the block size")
+	}
+
+	block, err := aes.NewCipher(keyByte)
+	if err != nil {
+		panic(err)
+	}
+	// The IV needs to be unique, but not secure. Therefore it's common to
+	// include it at the beginning of the ciphertext.
+	ciphertext := make([]byte, aes.BlockSize+len(plaintext))
+	iv := ciphertext[:aes.BlockSize]
+	//Reader is a global, shared instance of a cryptographically strong pseudo-random generator
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		panic(err)
+	}
+	//NewCBCEncrypter returns a BlockMode which encrypts in cipher block chaining mode
+	mode := cipher.NewCBCEncrypter(block, iv)
+	//CryptBlocks encrypts or decrypts a number of blocks.
+	mode.CryptBlocks(ciphertext[aes.BlockSize:], plaintext)
+
+	// It's important to remember that ciphertexts must be authenticated
+	// (i.e. by using crypto/hmac) as well as being encrypted in order to
+	// be secure.
+	//fmt.Printf("%x\n", ciphertext)
+
+	return hex.EncodeToString(ciphertext) 
+}
+
+func Decrypter(cipherText string, key string) string {
+	keyByte := []byte(key)
+	ciphertext, _ := hex.DecodeString(cipherText)
+
+	block, err := aes.NewCipher(keyByte)
+	if err != nil {
+		panic(err)
+	}
+
+	// The IV needs to be unique, but not secure. Therefore it's common to
+	// include it at the beginning of the ciphertext.
+	if len(ciphertext) < aes.BlockSize {
+		panic("ciphertext too short")
+	}
+	iv := ciphertext[:aes.BlockSize]
+	ciphertext = ciphertext[aes.BlockSize:]
+
+	// CBC mode always works in whole blocks.
+	if len(ciphertext)%aes.BlockSize != 0 {
+		panic("ciphertext is not a multiple of the block size")
+	}
+
+	mode := cipher.NewCBCDecrypter(block, iv)
+
+	// CryptBlocks can work in-place if the two arguments are the same.
+	mode.CryptBlocks(ciphertext, ciphertext)
+
+	// If the original plaintext lengths are not a multiple of the block
+	// size, padding would have to be added when encrypting, which would be
+	// removed at this point. For an example, see
+	// https://tools.ietf.org/html/rfc5246#section-6.2.3.2. However, it's
+	// critical to note that ciphertexts must be authenticated (i.e. by
+	// using crypto/hmac) before being decrypted in order to avoid creating
+	// a padding oracle.
+
+	fmt.Printf("%s\n", ciphertext)
+	return cipherText
+}
